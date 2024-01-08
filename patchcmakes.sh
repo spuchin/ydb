617f90a9855240616fbcbd6ca03354112a4062d5
Alexander Smirnov <alex@ydb.tech>
2024-01-08 18:15:30 +0300

    dummy.cpp everywhere!

---
+++ patchcmakes.sh
@@ -0,0 +1,8 @@
+find $1 -name 'CMakeLists.*.txt' | xargs grep -l add_library | xargs grep -L target_sources | grep -e '/llvm[0-9]*/'| while read x;do
+  echo "$x"
+  libname=`grep add_library $x | awk -F'(' '{print $2}' | awk -F'[ )]' '{print $1}'`
+  echo $libname
+  echo "target_sources( $libname PRIVATE dummy.cpp )" >> "$x"
+  touch $(dirname "$x")/dummy.cpp
+  echo
+done
